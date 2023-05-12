5e24782aad7f1f12188a42a924f9120da062bc96
Junior <junior@pclinux.juniorfox.net>
2023-05-12 16:35:07 -0300

    creation of i3exit

--- installation.sh
+++ installation.sh
@@ -22,7 +22,7 @@ sudo dnf install xrdp -y && \
 	sudo firewall-cmd --permanent --add-port=3389/tcp && sudo firewall-cmd --reload && \
 	sudo chcon --type=bin_t /usr/sbin/xrdp  && sudo chcon --type=bin_t /usr/sbin/xrdp-sesman 
 
-sudo dnf remove volumeicon xfce4-terminal
+sudo dnf remove -y volumeicon xfce4-terminal
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y org.flameshot.Flameshot com.valvesoftware.Steam com.visualstudio.code com.getpostman.Postman
@@ -37,3 +37,36 @@ sudo mkdir /usr/share/fonts/fontawesome6-brands-fonts/
 sudo cp  /tmp/fontawesome-free-6.4.0-desktop/otfs/Font\ Awesome\ 6\ Free-Regular-400.otf /usr/share/fonts/fontawesome6-free-fonts/
 sudo cp  /tmp/fontawesome-free-6.4.0-desktop/otfs/Font\ Awesome\ 6\ Free-Solid-900.otf /usr/share/fonts/fontawesome6-free-fonts/
 sudo cp  /tmp/fontawesome-free-6.4.0-desktop/otfs/Font\ Awesome\ 6\ Brands-Regular-400.otf /usr/share/fonts/fontawesome6-brands-fonts/
+rm -rf /tmp/fontawesome*
+cat << EOF | sudo tee /usr/local/bin/i3exit && sudo chmod +x /usr/local/bin/i3exit
+#!/bin/sh
+#https://www.vivaolinux.com.br/topico/Funtoo/Logout-Suspend-Reboot-e-Shutdown-no-i3wm
+lock() {
+    i3lock
+}
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        lock && systemctl suspend
+        ;;
+    hibernate)
+        lock && systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+exit 0
+EOF
+

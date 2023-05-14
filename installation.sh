43cde0d870a47512b0c72596433db31fae38b591
Carlos Junior <cjuniorfox@gmail.com>
2023-05-13 21:59:09 -0300

    replaced the jgmenu for the old rofi menu, new shutdown menu

--- installation.sh
+++ installation.sh
@@ -4,8 +4,8 @@ sudo dnf upgrade && \
 	sudo dnf update && \
 	sudo dnf install \
 	git \
-	i3-gaps dmenu xfce-polkit picom alacritty polybar jgmenu xdotool \
-	xfce4-power-manager nitrogen lxappearance adwaita-gtk2-theme adwaita-cursor-theme adwaita-blue-gtk-theme \
+	i3-gaps dmenu rofi xfce-polkit picom alacritty polybar jgmenu xdotool \
+	xfce4-power-manager lxappearance adwaita-gtk2-theme adwaita-cursor-theme adwaita-blue-gtk-theme \
 	akmod-nvidia mangohud xorg-x11-drv-nvidia-cuda nvidia-xconfig \
 	flatpak \
 	mozilla-openh264 \
@@ -15,7 +15,8 @@ sudo dnf upgrade && \
 	toolbox \
 	gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav \
 	--exclude=gstreamer1-plugins-bad-free-devel \
-	blueman
+	blueman \
+	gamemode
 
 sudo dnf install xrdp -y && \
 	sudo systemctl enable xrdp --now && \
@@ -25,7 +26,14 @@ sudo dnf install xrdp -y && \
 sudo dnf remove -y volumeicon xfce4-terminal
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y org.flameshot.Flameshot com.valvesoftware.Steam com.visualstudio.code com.getpostman.Postman
+
+flatpak install -y org.flameshot.Flameshot \
+	com.valvesoftware.Steam \
+	com.visualstudio.code \
+	com.getpostman.Postman \
+	org.freedesktop.Platform.VulkanLayer.MangoHud
+
+#Playing games with MangoHUD, steamdeck and so on: https://www.clubedohardware.com.br/forums/topic/1609909-configurando-a-steam-flatpak-discos-mangohud-gamemode-e-remote-play/
 
 #sudo dnf group install "Fedora Workstation"
 
@@ -44,7 +52,7 @@ cat << EOF | sudo tee /usr/local/bin/i3exit && sudo chmod +x /usr/local/bin/i3ex
 lock() {
     i3lock
 }
-case "$1" in
+case "\$1" in
     lock)
         lock
         ;;
@@ -64,7 +72,7 @@ case "$1" in
         systemctl poweroff
         ;;
     *)
-        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        echo "Usage: \$0 {lock|logout|suspend|hibernate|reboot|shutdown}"
         exit 2
 esac
 exit 0
---
+++ rofi/shutdown.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+LOCK=" Lock"
+LOGOUT=" Logout"
+SUSPEND=" Suspend"
+#HIBERNATE=" Hibernate"
+REBOOT=" Reboot"
+SHUTDOWN=" Shutdown"
+
+CONF=$(cat << EOF
+configuration{
+        show-icons:false;
+}
+#window {
+        y-offset: 32px;
+        width: 16em;
+        anchor: north east;
+        location: north east;
+}
+mainbox {
+        children: [listview];
+}
+listview{
+        border: 1px;
+        border-radius: 6px;
+        coluns:2;
+        lines:3;
+}
+EOF
+)
+
+case $(cat << EOF | rofi -dmenu -theme-str "$CONF"
+$LOCK
+$LOGOUT
+
+$SUSPEND
+$REBOOT
+$SHUTDOWN
+EOF
+) in
+"$LOCK")
+ i3exit lock;;
+"$LOGOUT")
+ i3exit logout;;
+"$SUSPEND")
+ i3exit suspend;;
+"$REBOOT")
+ i3exit reboot;;
+"$SHUTDOWN")
+ i3exit shutdown;;
+esac

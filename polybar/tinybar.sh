fcc986ef7f86ffb8436594da59a5920b1b9abd6c
Carlos Junior <cjuniorfox@gmail.com>
2023-06-12 15:11:09 -0300

    tray bar by user

--- polybar/tinybar.sh
+++ polybar/tinybar.sh
@@ -2,6 +2,6 @@
 
 #!/usr/bin/env bash
 
-echo "---" | tee -a /tmp/polybar2.log
+echo "---" | tee -a /tmp/$(whoami)-polybar2.log
 u=$(xprop -name "Polybar tray window" _NET_WM_PID | grep -o '[[:digit:]]*')
-kill $u || polybar tray >> /tmp/polybar2.log 2>&1
+kill $u || polybar tray >> /tmp/$(whoami)-polybar2.log 2>&1

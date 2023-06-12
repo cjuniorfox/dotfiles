72d596856887e5aa8a3dc6dc337a6fedd95e6919
Carlos Junior <cjuniorfox@gmail.com>
2023-06-12 16:33:10 -0300

    fix for close symbol

--- polybar/close.sh
+++ polybar/close.sh
@@ -2,7 +2,7 @@
 
 BEFORE=""; 
 while true ; do
-	PROP="$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW 2>/dev/null | cut -f 2)"
+	PROP="$((xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW) 2>/dev/null | cut -f 2 | grep -e '^[0-9a-f]x[0-9a-f]*[0-9a-f]$')"
 	if [[ "$PROP" != "0x0" && "$PROP" != "" ]] ; then
 		CLOSE="$1";
 	else

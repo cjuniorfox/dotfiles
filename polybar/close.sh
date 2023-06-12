723ddc38c96b99be8810a8b9dd84169773dad0bb
Carlos Junior <cjuniorfox@gmail.com>
2023-06-12 16:05:57 -0300

    fix for close symbol

--- polybar/close.sh
+++ polybar/close.sh
@@ -3,7 +3,11 @@
 BEFORE=""; 
 while true ; do
 	PROP="$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW 2>/dev/null | cut -f 2)"
-	CLOSE=$([[ ! "$PROP" -eq "0x0" ]] && echo $1 || echo ''); 
+	if [[ "$PROP" != "0x0" && "$PROP" != "" ]] ; then
+		CLOSE="$1";
+	else
+		CLOSE="";
+	fi
 	if [[  "$CLOSE" != "$BEFORE" ]]; then 
 		BEFORE="$CLOSE"; 
 		echo "$CLOSE"; 

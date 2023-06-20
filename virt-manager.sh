52232389b04f456672cedfb7a57b9266779f5148
Carlos Junior <cjuniorfox@gmail.com>
2023-06-20 17:41:16 -0300

    added virt manager fix

--- virt-manager.sh
+++ virt-manager.sh
@@ -22,3 +22,11 @@ nmcli connection show --active
 echo Enabling\ WoL
 nmcli con modify Wired\ Bridged\ \(br0\) 802-3-ethernet.wake-on-lan magic
 nmcli c show Wired\ Bridged\ \(br0\) | grep 802-3-ethernet.wake-on-lan
+
+#Fix for virt-manager losing connection after while
+#https://discussion.fedoraproject.org/t/virtual-machine-manager-showing-qemu-kvm-connecting/83961/5
+
+sudo tee /etc/sysconfig/virtnetworkd << EOF > /dev/null
+VIRTNETWORKD_ARGS=
+EOF
+sudo systemctl enable virtnetworkd.service --now

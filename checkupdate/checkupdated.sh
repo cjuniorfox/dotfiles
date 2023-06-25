d207764b7b6fca54324859cf94e3a14071eb1bdf
Carlos Junior <cjuniorfox@gmail.com>
2023-06-25 15:08:40 -0300

    Changes in checkupdate

--- checkupdate/checkupdated.sh
+++ checkupdate/checkupdated.sh
@@ -2,11 +2,19 @@
 
 . /usr/local/bin/checkupdate.sh getvariables
 
-mkfifo "$PIPE" -m 666 2> /dev/null
+touch "$PIPE" 2> /dev/null
+chmod 666 "$PIPE"
 
 SLEEP=7200
 
+PIPE_MAX_LINES=100
+
 while true; do
+	if [[ "$(wc -l $PIPE)" > "$PIPE_MAX_LINES" ]]; then
+		rm "$PIPE"
+		touch "$PIPE"
+		chmod 666 "$PIPE"
+	fi
 	echo  requesting update check
 	. /usr/local/bin/checkupdate.sh | tee -a "$PIPE"
 	echo Sleeping for "$SLEEP" seconds

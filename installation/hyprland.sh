ea1cbf586edc4aac03af967f9dcaa8663291c4ca
Junior_FOX <Junior_FOX>
2023-07-25 20:36:43 -0300

    refactor hyprland

--- installation/hyprland.sh
+++ installation/hyprland.sh
@@ -1,37 +1,55 @@
-#!/bin/bash
-sudo dnf upgrade && \
-	sudo dnf update && \
-	sudo dnf groupinstall Fonts Administrative\ Tools Hardware\ Support && \
-	sudo dnf install \
-	git \
-	sddm rofi-wayland xfce-polkit picom kitty wob \
-	xfce4-settings adwaita-gtk2-theme adwaita-cursor-theme adwaita-blue-gtk-theme \
+sudo dnf upgrade -y && \
+	sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" && \
+	sudo dnf install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" && \
+        sudo dnf copr enable solopasha/hyprland && \
+	sudo dnf update -y && \
+	sudo dnf remove firefox xdg-desktop-portal-wlr -y && \
+	sudo dnf install adwaita-blue-gtk-theme \
+	adwaita-gtk2-theme \
+	blueman \
+	brightnessctl \
 	flatpak \
-	mozilla-openh264 \
+	git \
+	gvfs-smb \
+	hyprland \
+	hyprland-contrib \
+	hyprland-plugins \
+	hyprpaper \
+	hyprpicker \
+	hyprshot \
+	kitty \
+	nautilus \
+	network-manager-applet \
+	pavucontrol \
+	polkit-gnome \
+	pulseaudio-utils \
+	rofi-wayland \
+	rpmfusion-free-release \
+	rpmfusion-nonfree-release \
+	steam-devices \
+	swaybg \
+	swayidle \
+	swaylock \
 	toolbox \
-	gvfs-smb nautilus vim \
-	toolbox \
-	gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav \
-	--exclude=gstreamer1-plugins-bad-free-devel \
-	blueman
+	vim-enhanced \
+	waybar-hyprland \
+	wlr-randr \
+	wob \
+	xdg-desktop-portal-hyprland
 
-sudo dnf copr enable solopasha/hyprland && \
-	sudo dnf install hyprland hyprland-contrib hyprland-plugins hyprpaper hyprshot waybar-hyprland xdg-desktop-portal-hyprland \
-	aajohan-comfortaa-fonts swayidle swaylock wlr-randr
-
-#sudo dnf remove -y lightdm volumeicon xfce4-terminal thunar
-
-sudo systemctl enable sddm
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-delete fedora && \
+	sudo flatpak remote-delete fedora-testing && \
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y \
-	com.visualstudio.code \
-	com.getpostman.Postman \
 	com.github.hluk.copyq \
-	org.gnome.FileRoller \
 	com.github.tchx84.Flatseal \
-	org.openrgb.OpenRGB
+	org.gnome.Calculator \
+	org.gnome.FileRoller \
+	org.gnome.FontManager \
+	org.mozilla.firefox \
+	org.freedesktop.Platform.ffmpeg-full \
+	org.freedesktop.Platform.openh264
 
 wget "https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-desktop.zip" -O /tmp/fontawesome-free-6.4.0-desktop.zip
 unzip /tmp/fontawesome-free-6.4.0-desktop.zip -d /tmp/
@@ -42,6 +60,7 @@ sudo cp  /tmp/fontawesome-free-6.4.0-desktop/otfs/Font\ Awesome\ 6\ Free-Regular
 sudo cp  /tmp/fontawesome-free-6.4.0-desktop/otfs/Font\ Awesome\ 6\ Free-Solid-900.otf /usr/share/fonts/fontawesome6-free-fonts/
 sudo cp  /tmp/fontawesome-free-6.4.0-desktop/otfs/Font\ Awesome\ 6\ Brands-Regular-400.otf /usr/share/fonts/fontawesome6-brands-fonts/
 rm -rf /tmp/fontawesome*
+
 cat << EOF | sudo tee /usr/local/bin/i3exit && sudo chmod +x /usr/local/bin/i3exit
 #!/bin/sh
 #https://www.vivaolinux.com.br/topico/Funtoo/Logout-Suspend-Reboot-e-Shutdown-no-i3wm
@@ -73,9 +92,3 @@ case "\$1" in
 esac
 exit 0
 EOF
-cat << EOF | sudo tee /usr/local/bin/code && sudo chmod +x /usr/local/bin/code
-#!/bin/bash
-flatpak run com.visualstudio.code \$1 \$2 \$3 \$4 \$5
-EOF
-
-sudo timedatectl set-local-rtc false

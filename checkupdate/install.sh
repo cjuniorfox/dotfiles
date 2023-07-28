5b12027d00fe07347982f9e073c22d4a7f0d48d2
Junior_FOX <Junior_FOX>
2023-07-28 18:54:51 -0400

    checkupdate as a rpm package prepare

--- checkupdate/install.sh
+++ checkupdate/install.sh
@@ -2,9 +2,9 @@
 
 cat << EOF | pkexec 
     cd "$(pwd)"
-    cp checkupdated.sh /usr/local/bin
-    cp checkupdate.sh /usr/local/bin
-    cp update.sh /usr/local/bin
+    cp checkupdated /usr/local/bin
+    cp checkupdate /usr/local/bin
+    cp update /usr/local/bin
     cp checkupdate.service /etc/systemd/system/
     systemctl daemon-reload
     systemctl enable checkupdate --now
